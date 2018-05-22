35128bfdee90fe17ad5e19b589f1605318d7ae5b
Adrian Garza <adrianhgarza@gmail.com>
2018-05-22 23:55:59 +0100

    first commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	//public constants
+	public float MaxMoveSpeed;
+
+	//physics
+	Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Horizontal") != 0) {
+			rb2d.velocity = new Vector2(x:(Input.GetAxis("Horizontal") * MaxMoveSpeed), y:0);
+		} else {
+			rb2d.velocity = new Vector2(x:0, y:rb2d.velocity.y);
+		}
+	}
+}
---
+++ Assets/Scripts/Global/PixelSnapper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+[ExecuteInEditMode]
+public class PixelSnapper : MonoBehaviour {
+    [Range(1, 4)]
+    public int pixelScale = 1;
+
+    private Camera _camera;
+
+    void Update()
+    {
+        if (_camera == null)
+        {
+            _camera = GetComponent<Camera>();
+            _camera.orthographic = true;
+        }
+        _camera.orthographicSize = Screen.height * (0.005f / pixelScale);
+    }
+}
