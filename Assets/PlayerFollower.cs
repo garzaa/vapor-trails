5119a47655d49d2a92b58c9fec1b677f07c60c29
Adrian Garza <adrianhgarza@gmail.com>
2018-05-23 17:53:51 +0100

    smooth camera

---
+++ Assets/PlayerFollower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour {
+
+	public GameObject player;
+	public float moveSpeed;
+	Vector3 velocity = Vector3.zero;
+
+	void Start () {
+		this.transform.position = new Vector3(x:player.transform.position.x,
+			y:player.transform.position.y,
+			z:this.transform.position.z
+		);
+	}
+	
+	void FixedUpdate() {
+		transform.position = Vector3.SmoothDamp(
+			transform.position,
+			new Vector3(
+				x:player.transform.position.x,
+				y:player.transform.position.y,
+				z:this.transform.position.z),
+			ref velocity,
+			moveSpeed * Time.deltaTime
+			);
+	}
+}
