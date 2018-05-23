1c4c127b3d55a3698e7865a4e05028d079af1c80
Adrian Garza <adrianhgarza@gmail.com>
2018-05-23 17:34:22 +0100

    she Jump

---
+++ Assets/Scripts/Global/Entity.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+
+    [HideInInspector] public bool facingRight = true;
+    [HideInInspector] public bool movingRight = false;
+    public bool frozen = false;
+    public bool lockedInSpace = false;
+
+    public GameObject dustSprite;
+
+    //being lazy is valid :^)
+    public void Log(string str)
+    {
+        Debug.Log(str);
+    }
+    
+    public void Flip() 
+	{
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        //flip by scaling -1
+    }
+
+    public void Destroy() {
+        Destroy(this.gameObject);
+    }
+
+    public void CheckFlip() {
+        Rigidbody2D rb2d;
+        if ((rb2d = GetComponent<Rigidbody2D>()) != null) {
+            if (!facingRight && rb2d.velocity.x > 0 && movingRight)
+            {
+                Flip();
+            }
+            else if (facingRight && rb2d.velocity.x < 0 && !movingRight)
+            {
+                Flip();
+            }
+        }
+    }
+
+    public void LockInSpace() {
+        Rigidbody2D rb2d;
+        if ((rb2d = GetComponent<Rigidbody2D>()) != null) {
+            rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
+            this.lockedInSpace = true;
+        }
+    }
+
+    public void UnLockInSpace() {
+        Rigidbody2D rb2d;
+        if ((rb2d = GetComponent<Rigidbody2D>()) != null) {
+            rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+            this.lockedInSpace = false;
+        }
+    }
+
+    //returns the x-direction the entity is facing
+    public int GetForwardScalar() {
+        return facingRight ? 1 : -1;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Entity {
+
+	//public constants
+	float MaxMoveSpeed = 2;
+	float JumpSpeed = 4;
+	float JUMP_CUTOFF = 2.0f;
+
+	//physics
+	Rigidbody2D rb2d;
+
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+		this.facingRight = false;
+	}
+	
+	void Update () {
+		Move();
+		Jump();
+	}
+
+	void Move() {
+		if (HorizontalInput()) {
+			if (Input.GetAxis("Horizontal") != 0) {
+				rb2d.velocity = new Vector2(x:(Input.GetAxis("Horizontal") * MaxMoveSpeed), y:rb2d.velocity.y);
+			}
+		} 
+		//if no movement, stop the player on the ground 
+		else {
+			rb2d.velocity = new Vector2(x:0, y:rb2d.velocity.y);
+		}
+
+		//flip sprites depending on movement direction
+        if (!facingRight && rb2d.velocity.x > 0)
+        {
+            Flip();
+        }
+        else if (facingRight && rb2d.velocity.x < 0)
+        {
+            Flip();
+        }
+	}
+
+	void Jump() {
+		if (Input.GetButtonDown("Jump")) {
+			rb2d.velocity = new Vector2(x:rb2d.velocity.x, y:JumpSpeed);
+		}
+		//emulate an analog jump
+        //if the jump button is released
+        if (Input.GetButtonUp("Jump") && rb2d.velocity.y > JUMP_CUTOFF) {
+            //then decrease the y velocity to the jump cutoff
+            rb2d.velocity = new Vector2(rb2d.velocity.x, JUMP_CUTOFF);
+        }
+	}
+
+	bool HorizontalInput() {
+		return Input.GetAxis("Horizontal") != 0;
+	}
+}
