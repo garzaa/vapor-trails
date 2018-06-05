9a5038c8a4673ba24a7ad201db74de6c6778cdf0
Adrian Garza <adrianhgarza@gmail.com>
2018-06-05 18:14:28 +0200

    sign ui showing/hiding

---
+++ Assets/Scripts/UI/SignUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SignUI : UIComponent {
+
+	public Text signTextHidden;
+	public Text signTextShown;
+
+	Animator anim;
+
+	public override void Show() {
+		anim.SetBool("Visible", true);
+	}
+
+	public override void Hide() {
+		anim.SetBool("Visible", false);
+	}
+
+	public void SetText(string text) {
+		signTextHidden.text = text;
+		signTextShown.text = text;
+	}
+}
