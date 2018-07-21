3531d6416feb799f0a3a34d369bbf1512497bb74
garzaa <adrianhgarza@gmail.com>
2018-07-20 20:17:29 -0700

    camera grabber

---
+++ Assets/Scripts/Global/CameraGrabber.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraGrabber : PlayerTriggeredObject {
+
+	public override void OnPlayerEnter() {
+		GlobalController.playerFollower.DisableFollowing();
+	}
+
+	public override void OnPlayerExit() {
+		GlobalController.playerFollower.EnableFollowing();
+	}
+}
