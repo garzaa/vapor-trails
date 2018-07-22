6687f03083ff5bc00ab9212fc45b1c63873477fd
garzaa <adrianhgarza@gmail.com>
2018-07-22 13:08:37 -0700

    false walls

---
+++ Assets/Scripts/Global/Triggered Objects/FalseWall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class FalseWall : PlayerTriggeredObject {
+
+	public override void OnPlayerEnter() {
+		GetComponent<Animator>().SetBool("Hidden", true);
+	}
+
+	public override void OnPlayerExit() {
+		GetComponent<Animator>().SetBool("Hidden", false);
+	}
+}
