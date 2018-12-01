aa07fdf37a26a5eaec1a04a5b030c6462853400a
garzaa <adrianhgarza@gmail.com>
2018-11-30 20:36:48 -0800

    animation fixes

---
+++ Assets/FreezePlayerForDuration.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezePlayerForDuration : StateMachineBehaviour {
+
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		animator.GetComponent<PlayerController>().Freeze();
+	}
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		animator.GetComponent<PlayerController>().UnFreeze();
+	}
+}
