1d5fccd880a8ea46a4b383b3b43c56c11c357890
garzaa <adrianhgarza@gmail.com>
2019-01-01 16:46:59 -0800

    service elevator beginnings

---
+++ Assets/Scripts/Game States/Activation Criteria/PlayerEnterCriteria.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerEnterCriteria : ActivationCriteria {
+
+	bool containsPlayer = false;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag(Tags.Player)) {
+			this.containsPlayer = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.CompareTag(Tags.Player)) {
+			this.containsPlayer = true;
+		}
+	}
+
+	override public bool CheckSatisfied() {
+		return containsPlayer;
+	}
+
+}
