b2c20062689037adfa8d03452175d69f0ef1b0b4
garzaa <adrianhgarza@gmail.com>
2019-01-05 19:32:43 -0800

    enabler/disabler

---
+++ Assets/Scripts/Game States/Activatables/EnablerDisabler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablerDisabler : Activatable {
+
+	public GameObject target;
+
+	public override void ActivateSwitch(bool b) {
+		target.SetActive(!target.activeSelf);
+	}
+}
