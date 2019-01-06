60361606938a1bda2b2e40e4ba71bdf64b2e73a0
garzaa <adrianhgarza@gmail.com>
2019-01-05 19:29:22 -0800

    multi-activatable

---
+++ Assets/Scripts/Game States/Activatables/MultiActivatable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiActivatable : Activatable {
+	public List<Activatable> activatables;
+
+	public override void ActivateSwitch(bool b) {
+		foreach(Activatable a in activatables) {
+			a.gameObject.SetActive(b);
+		}
+	}
+}
