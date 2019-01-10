70748300e636e1207dd20c5741113c68b7e2d8da
garzaa <adrianhgarza@gmail.com>
2019-01-09 22:34:06 -0800

    player facer

---
+++ Assets/Scripts/One-Offs/FaceTarget.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceTarget : MonoBehaviour {
+
+	public bool facingRight;
+	public GameObject target;
+
+	void Start() {
+		if (target == null) {
+			target = GlobalController.pc.gameObject;
+		}
+	}
+
+	void Update() {
+		if (facingRight) {
+			if (target.transform.position.x < this.transform.position.x) {
+				Flip();
+			}
+		} else {
+			if (target.transform.position.x > this.transform.position.x) {
+				Flip();
+			}
+		}
+	}
+
+	void Flip() {
+		this.transform.localScale = new Vector2(this.transform.localScale.x * -1, this.transform.localScale.y);
+		facingRight = !facingRight;
+	}
+
+}
