908b85027a95b0ccedc9e1942c6fc693593329bf
garzaa <adrianhgarza@gmail.com>
2019-01-21 14:34:45 -0800

    persistent enabled

---
+++ Assets/Scripts/Game States/PersistentObjects/PersistentEnabled.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentEnabled : PersistentObject {
+
+	bool firstEnable = true;
+
+	void OnEnable() {
+		if (firstEnable) {
+			firstEnable = false;
+			return;
+		} else {
+			SerializedPersistentObject o = LoadObjectState();
+			if (o != null) {
+				bool enabled = (bool) o.persistentProperties["enabled"];
+				this.gameObject.SetActive(enabled);
+				UpdateObjectState();
+			}
+		}
+	}
+
+	void OnDisable() {
+		UpdateObjectState();
+	}
+
+	public override void Start() {
+		OnEnable();
+	}
+
+	protected override void UpdateObjectState() {
+		persistentProperties = new Hashtable();
+		persistentProperties.Add("enabled", this.enabled);
+		SaveObjectState();
+	}
+	
+}
