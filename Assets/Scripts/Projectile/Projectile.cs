332d2f0470a67c101c7d863473a75bcb127f4827
garzaa <adrianhgarza@gmail.com>
2019-02-03 14:51:27 -0800

    bullet impact reflected across normals

--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -7,10 +7,13 @@ public class Projectile : MonoBehaviour {
 	public bool impactShake;
 	public GameObject burstPrefab;
 
+	GameObject impactDust;
+
 	public bool ignorePlayer = false;
 
 	void Start() {
 		this.transform.parent = null;
+		impactDust = (GameObject) Resources.Load("ImpactDustPrefab");
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
@@ -25,13 +28,23 @@ public class Projectile : MonoBehaviour {
 		if (impactShake) {
 			CameraShaker.TinyShake();
 		}
-		OnImpact();
+		OnImpact(other);
 	}
 
-	public void OnImpact() {
+	public void OnImpact(Collider2D other) {
 		if (burstPrefab != null) {
 			GameObject go = Instantiate(burstPrefab, transform.position, Quaternion.identity);
 		}
+
+		RaycastHit2D hit = Physics2D.Raycast(this.transform.position, other.transform.position, 4, 1 << LayerMask.NameToLayer(Layers.Ground));
+		if (hit.transform != null) {
+			Debug.Log("PINGAS");
+			Vector2 originalMotion = this.GetComponent<Rigidbody2D>().velocity;
+			Vector2 flipped = Vector2.Reflect(originalMotion, hit.normal);
+			float newAngle = Vector2.Angle(Vector2.left, flipped);
+			GameObject g = (GameObject) Instantiate(impactDust, hit.point, Quaternion.Euler(0, 0, newAngle+180), null);
+		}
+
 		GetComponent<Rigidbody2D>().velocity = Vector3.zero;
 		SoundManager.ExplosionSound();
 		//remove the collider/sprites/etc and stop particle emission
