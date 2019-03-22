efc7959a1cd676345889cde777701c32f8cb4bd9
garzaa <adrianhgarza@gmail.com>
2019-03-21 23:43:58 -0700

    platform mover

---
+++ Assets/Scripts/Global/MovingObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour {
+
+	public GameObject mainObject;
+	public GameObject pointA;
+	public GameObject pointB;
+	public float moveSpeed = 1;
+
+	private bool returning = false;
+
+	void Start () {
+		if (pointA == null || pointB == null) {
+			Debug.LogError("You forgot to assign the movement endpoints!");
+		}
+		mainObject.transform.position = pointA.transform.position;
+	}
+
+	//moves from point A to point B and then back	
+	void Update () {
+		Move();
+		if (!returning) {
+			if (mainObject.transform.position == pointB.transform.position) {
+				returning = true;
+			}
+		} else {
+			if (mainObject.transform.position == pointA.transform.position) {
+				returning = false;
+			}
+		}
+	}
+
+	void Move() {
+		Vector3 target = returning ? pointA.transform.position : pointB.transform.position;
+		float step = moveSpeed * Time.deltaTime;
+        mainObject.transform.position = Vector3.MoveTowards(mainObject.transform.position, target, step);
+	}
+}
