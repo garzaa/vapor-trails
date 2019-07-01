9f199533f1816f2be10a46a9989eb821c742eb6a
garzaa <adrianhgarza@gmail.com>
2019-06-30 17:30:33 -0700

    speed jabs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : Entity {
 	float jumpSpeed = 4.5f;
 	float jumpCutoff = 2.0f;
 	float hardLandSpeed = -4.5f;
-	public float dashSpeed = 8f;
+	public float dashSpeed = 6f;
 	float terminalSpeed = -10f;
 	float superCruiseSpeed = 12f;
 	float dashCooldownLength = .5f;
