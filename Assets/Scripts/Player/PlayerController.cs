c8f5dc2751d4d79150c725b7de2f73012077b823
garzaa <adrianhgarza@gmail.com>
2019-07-01 21:45:26 -0700

    fixed divide by zero error

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1023,6 +1023,7 @@ public class PlayerController : Entity {
 
 	public float MoveSpeedRatio() {
 		if (rb2d == null) return 0;
+		if (speedLimiter.maxSpeedX == 0) return 0;
 		return Mathf.Abs(rb2d.velocity.x / speedLimiter.maxSpeedX);
 	}
 
