c98ae493efe49c8db6bcf3eaf5bd6fc2a5a41965
Adrian Garza <adrianhgarza@gmail.com>
2019-07-28 12:50:59 -0700

    added sprite mask links to NPC rig

--- Assets/Scripts/Visual/SpriteMaskLink.cs
+++ Assets/Scripts/Visual/SpriteMaskLink.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[RequireComponent(typeof(SpriteMask))]
 public class SpriteMaskLink : MonoBehaviour {
     public SpriteRenderer otherSpriteRenderer;
     SpriteMask mask;
