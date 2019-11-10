e0b2dfcd2383974bf0147a70f2cf054cb319c696
Adrian Garza <adrianhgarza@gmail.com>
2019-11-10 12:41:51 -0800

    update prep

--- Assets/Scripts/Game States/Activatables/RandomEnabler.cs
+++ Assets/Scripts/Game States/Activatables/RandomEnabler.cs
@@ -6,6 +6,6 @@ public class RandomEnabler : Activatable {
     public GameObject[] choices;
 
     override public void ActivateSwitch(bool b) {
-        choices[Random.Range(0, choices.Length)].active = true;
+        choices[Random.Range(0, choices.Length)].SetActive(true);
     }
 }
