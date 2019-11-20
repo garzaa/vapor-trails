f572fdd84c10f9f0656cf56fe24895fc7609edb2
Adrian Garza <adrianhgarza@gmail.com>
2019-11-20 14:55:35 -0800

    icon poc

--- Assets/Scripts/Enums/Layers.cs
+++ Assets/Scripts/Enums/Layers.cs
@@ -15,6 +15,7 @@ public abstract class Layers {
 	public const string Interactables = "Interactables";
 	public const string Breakables = "Breakables";
 	public const string SpriteMasks = "Sprite Masks";
+	public const string Minimap = "Minimap";
 
 	public const int DefaultMask = 1;
 	public const int TransparentFXMask = 1 << 1;
@@ -31,6 +32,7 @@ public abstract class Layers {
 	public const int InteractablesMask = 1 << 15;
 	public const int BreakablesMask = 1 << 16;
 	public const int SpriteMasksMask = 1 << 17;
+	public const int MinimapMask = 1 << 18;
 
 	public const int DefaultNumber = 0;
 	public const int TransparentFXNumber = 1;
@@ -47,4 +49,5 @@ public abstract class Layers {
 	public const int InteractablesNumber = 15;
 	public const int BreakablesNumber = 16;
 	public const int SpriteMasksNumber = 17;
+	public const int MinimapNumber = 18;
 }
