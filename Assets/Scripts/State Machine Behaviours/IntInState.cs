1c476f54bb39d1485f2e400b0cfe85f79f3747a5
garzaa <adrianhgarza@gmail.com>
2019-12-26 19:39:38 -0800

    updated intInState

--- Assets/Scripts/State Machine Behaviours/IntInState.cs
+++ Assets/Scripts/State Machine Behaviours/IntInState.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class IntInState : StateMachineBehaviour {
 
     public int stateNum;
+    public bool isStateMachine;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        if (isStateMachine) return;
         animator.SetInteger("SubState", stateNum);
+    }
+
+    override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash) {
+        if (isStateMachine) animator.SetInteger("SubState", stateNum);
     }
 
 }
