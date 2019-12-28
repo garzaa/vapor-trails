d8c5979a0136962d1af3214a5008c9064266df93
garzaa <adrianhgarza@gmail.com>
2019-12-27 18:31:56 -0800

    updated substate int setters

--- Assets/Scripts/State Machine Behaviours/IntInState.cs
+++ Assets/Scripts/State Machine Behaviours/IntInState.cs
@@ -5,21 +5,26 @@ public class IntInState : StateMachineBehaviour {
     public int stateNum;
     public bool isStateMachine;
 
+    int lastEntryState = 0;
+
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         if (isStateMachine) return;
+        lastEntryState = animator.GetInteger("SubState");
         animator.SetInteger("SubState", stateNum);
     }
 
     override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash) {
-        if (isStateMachine) animator.SetInteger("SubState", stateNum);
+        if (!isStateMachine) return;
+        animator.SetInteger("SubState", stateNum);
+        lastEntryState = animator.GetInteger("SubState");
     }
 
 
     override public void OnStateMachineExit(Animator animator, int stateMachinePathHash) {
-        if (isStateMachine) animator.SetInteger("SubState", -1);
+        if (isStateMachine) animator.SetInteger("SubState", lastEntryState);
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        if (!isStateMachine) animator.SetInteger("SubState", -1);
+        if (!isStateMachine) animator.SetInteger("SubState", lastEntryState);
     }
 }
