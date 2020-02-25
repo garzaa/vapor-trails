e85e6847d855ee74c37b77b09d58b2211c2578f2
Adrian Garza <adrianhgarza@gmail.com>
2020-02-24 23:00:49 -0800

    downtown color sketch

--- Assets/Scripts/Visual/SpriteOrderOffset.cs
+++ Assets/Scripts/Visual/SpriteOrderOffset.cs
@@ -8,6 +8,9 @@ public class SpriteOrderOffset : MonoBehaviour {
     public int front;
     public int back;
 
+    [Header("Current sorting layer")]
+    public string layer;
+
     private Renderer[] renderers;
 
     public void Start() {
@@ -21,11 +24,17 @@ public class SpriteOrderOffset : MonoBehaviour {
     public void ApplyOffset() {
         int max = int.MinValue;
         int min = int.MaxValue;
+        bool setLayer = !string.IsNullOrEmpty(layer);
         for (int i=0; i<renderers.Length; i++) {
             Renderer s = renderers[i];
             s.sortingOrder += offset;
             max = s.sortingOrder > max ? s.sortingOrder : max;
             min = s.sortingOrder < min ? s.sortingOrder : min;
+            if (setLayer) {
+                s.sortingLayerName = layer;
+            } else {
+                layer = s.sortingLayerName;
+            }
         }
         front = max;
         back = min;
