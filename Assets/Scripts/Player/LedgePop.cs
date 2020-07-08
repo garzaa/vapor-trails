88d78c2d4f32e1fbb1735bfea196b32ad1c84c24
Adrian Hamilton Garza <adrian.hamilton.garza@autodesk.com>
2020-07-07 23:28:17 -0700

    generalized ledge pop

--- Assets/Scripts/Player/LedgePop.cs
+++ Assets/Scripts/Player/LedgePop.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 
 public class LedgePop : MonoBehaviour {
     const float popDistance = 0.10f;
-    const float castDistance = 0.10f;
+    const float castDistance = 0.05f;
+    const float speedMultiplier = 0.5f;
 
     BoxCollider2D box;
     Rigidbody2D rb;
 
     int layerMask;
+
+    Vector2 boxPos;
 
     void Start() {
         box = GetComponent<BoxCollider2D>();
@@ -19,43 +22,53 @@ public class LedgePop : MonoBehaviour {
     }
 
     void Update() {
-        if (rb.velocity.x == 0) return;
+        if (Mathf.Abs(rb.velocity.x) < 0.2f) return;
 
-        Vector2 boxPos = (Vector2) box.transform.position + box.offset;
+        boxPos = (Vector2) box.transform.position + box.offset;
 
-        // lower tolerance
-        Vector2 popDirection = Vector2.up;
+        CheckPopX();
+    }
 
-        // first: project the collider forward
-        Vector2 forward = Vector2.right * Mathf.Sign(rb.velocity.x);
+    void CheckPopX() {
+        float speed = rb.velocity.x;
+        Vector2 forward = Vector2.right * Mathf.Sign(speed);
+        float distance = (box.size.x/2f)+(castDistance * speed * speedMultiplier);
 
+        CheckPop(Vector2.up, forward, distance);
+    }
+
+    void CheckPop(Vector2 popDirection, Vector2 forward, float distance) {
+        // first: project the collider forward to mimic movement
         RaycastHit2D hit = Physics2D.BoxCast(
             origin: boxPos,
+            // don't pick up what the box is currently touching
             size: box.size - new Vector2(0.01f, 0.01f),
             angle: 0,
             direction: forward,
-            distance: (box.size.x/2f)+castDistance,
+            distance: distance,
             layerMask: layerMask 
         );
 
-        // if no hit, return
+        // if no upcoming hit, who cares
         if (hit.transform == null) return;
         
-        // if hit, then cast in the same direction with lower tolerance applied
+        // if upcoming hit, then cast in the same direction with lower tolerance applied
         Vector2 pop = popDirection*popDistance;
+
         hit = Physics2D.BoxCast(
             origin: boxPos + pop*2,
             size: box.size - pop,
             angle: 0,
             direction: forward,
-            distance: (box.size.x/2f)+castDistance,
+            distance: distance,
             layerMask: layerMask
         );
 
 
-        // if no hit with tolerance, then pop the rb2d in that direction
+        // if no hit with inner tolerance, then pop the rb2d in that direction
         if (hit.transform == null) {
             rb.MovePosition(rb.position+pop);
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
         }
     }
 }
