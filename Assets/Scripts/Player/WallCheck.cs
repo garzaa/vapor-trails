a2f39170a9c528c0e809e1c059b7290ac558d97d
Adrian Garza <adrianhgarza@gmail.com>
2020-07-27 13:07:53 -0700

    finally fixed fuckign wall check

--- Assets/Scripts/Player/WallCheck.cs
+++ Assets/Scripts/Player/WallCheck.cs
@@ -9,79 +9,72 @@ public class WallCheck : MonoBehaviour {
 	const bool drawDebug = true;
 
 	public bool touchingWall;
-	ContactFilter2D filter;
 
-	int numHits;
-	RaycastHit2D[] hits = new RaycastHit2D[1];
-	RaycastHit2D hit;
+	int layerMask;
 
-	const float extendDistance = 0.01f;
-	// TODO: what the fuck? redo this the right way with normals logging
-	// ok so normals are either (-1, 0) for a left wall or (1, 0) for a right wall
-	// what the fuck angle does that translate to
-	// ok it's the angle against the cast distance
-	const float normalTolerance = 5f;
+	const float extendDistance = 0.03f;
 
-	void Start() {
-		filter = new ContactFilter2D();
-		filter.layerMask = 1 << LayerMask.NameToLayer(Layers.Ground);
-		filter.useLayerMask = true;
+	void Start() {		
+		layerMask = 1 << LayerMask.NameToLayer(Layers.Ground);
 	}
 
 	public WallCheckData GetWall() {
 		Vector2 startPoint = (Vector2) targetCollider.transform.position + targetCollider.offset;
 		Vector2 actualSize = new Vector2(targetCollider.size.x, targetCollider.bounds.size.y-(2*groundGap));
 
-		Debug.DrawLine(startPoint+actualSize/2, startPoint-actualSize/2, Color.blue);
-
 		float distance = targetCollider.size.x/2f + extendDistance;
 
-		// cast left and right
-		// filter.SetNormalAngle(180-normalTolerance, 180+normalTolerance);
-		numHits = Physics2D.BoxCast(startPoint, actualSize, 0, Vector2.left, filter, hits, distance);
-		if (drawDebug) {
-			// top edge
-			Debug.DrawLine(
-				new Vector2(startPoint.x, startPoint.y + (targetCollider.size.y/2f)),
-				new Vector2(startPoint.x-distance, startPoint.y + (targetCollider.size.y/2f)),
-				Color.red
-			);
-			// bottom edge
-			Debug.DrawLine(
-				new Vector2(startPoint.x, startPoint.y - (targetCollider.size.y/2f)),
-				new Vector2(startPoint.x-distance, startPoint.y - (targetCollider.size.y/2f)),
-				Color.red
-			);
-		}
-		if (numHits != 0) {
-			hit = hits[0];
+		Vector2 topStart = startPoint+(Vector2.up*actualSize.y*0.5f);
+		Vector2 bottomStart = startPoint+(Vector2.down*actualSize.y*0.5f);
+
+		//cast left
+		RaycastHit2D topHit = Physics2D.Raycast(
+			origin: topStart,
+			direction: Vector2.left,
+			distance: distance,
+			layerMask: layerMask
+		);
+		RaycastHit2D bottomHit = Physics2D.Raycast(
+			origin: bottomStart,
+			direction: Vector2.left,
+			distance: distance,
+			layerMask: layerMask
+		);
+		Debug.DrawLine(topStart, topStart+(Vector2.left*distance), Color.cyan);
+		Debug.DrawLine(bottomStart, bottomStart+(Vector2.left*distance), Color.cyan);
+		if (topHit.collider!=null && bottomHit.collider!=null) {
 			touchingWall = true;
 			return new WallCheckData(
-				Vector2.Distance(startPoint, hit.transform.position),
+				Vector2.Distance(startPoint, topHit.point),
 				-1
 			);
 		}
 
-		// right
-		// filter.SetNormalAngle(180-normalTolerance, 180+normalTolerance);
-		numHits = Physics2D.BoxCast(startPoint, actualSize, 0, Vector2.right, filter, hits, distance);
-		if (drawDebug) Debug.DrawLine(startPoint, startPoint + Vector2.right*(actualSize.x/2f+distance), Color.green);
-		if (numHits != 0) {
-			hit = hits[0];
+		// cast right
+		topHit = Physics2D.Raycast(
+			origin: startPoint+(Vector2.up*actualSize.y*0.5f),
+			direction: Vector2.right,
+			distance: distance,
+			layerMask: layerMask
+		);
+		bottomHit = Physics2D.Raycast(
+			origin: startPoint+(Vector2.down*actualSize.y*0.5f),
+			direction: Vector2.right,
+			distance: distance,
+			layerMask: layerMask
+		);
+		if (topHit.collider!=null && bottomHit.collider!=null) {
 			touchingWall = true;
-			if (drawDebug) {
-				Debug.DrawLine(startPoint, hit.transform.position, Color.magenta);
-			}
 			return new WallCheckData(
-				Vector2.Distance(startPoint, hit.transform.position),
+				Vector2.Distance(startPoint, topHit.point),
 				1
 			);
 		}
-
 		touchingWall = false;
 		return null;
 	}
 }
+
 
 public class WallCheckData {
 	public float distance;
