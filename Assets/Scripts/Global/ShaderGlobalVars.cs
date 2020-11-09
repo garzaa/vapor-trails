d3c19c9a927bedc5492d4abc78626c0264d7af01
Adrian Garza <adrianhgarza@gmail.com>
2020-11-08 16:59:08 -0800

    step time shaders

--- Assets/Scripts/Global/ShaderGlobalVars.cs
+++ Assets/Scripts/Global/ShaderGlobalVars.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
+using System.Collections;
 
 [ExecuteInEditMode]
 public class ShaderGlobalVars : MonoBehaviour {
+    void Start() {
+        StartCoroutine(UpdateStepTime());
+    }
+
     void Update() {
         Shader.SetGlobalFloat("_UnscaledTime", Time.unscaledTime);
+    }
+
+    IEnumerator UpdateStepTime() {
+        Shader.SetGlobalFloat("_StepTime", Time.time);
+        yield return new WaitForSeconds(1f/16f);
+        StartCoroutine(UpdateStepTime());
     }
 }
