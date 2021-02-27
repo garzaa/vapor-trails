cb49fc81b23ae7d70ecc531da9bce2c988d068cf
Adrian Garza <adrianhgarza@gmail.com>
2021-02-27 12:53:38 -0800

    optimized enemy player detection

--- Assets/Scripts/Enemies/State Machines/PlayerSensor.cs
+++ Assets/Scripts/Enemies/State Machines/PlayerSensor.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerSensor : Sensor {
 
 	public bool absoluteX = true;
+	Vector2 playerDist;
+
+	public bool running = false;
 
 	void Update() {
-		Vector2 playerDist = player.transform.position - this.transform.position;
-		if (pc.IsDead()) {
+		if (pc.IsDead() || !running) {
 			playerDist = new Vector2(10000, 10000);
+		} else {
+			playerDist = player.transform.position - this.transform.position;
 		}
 		animator.SetFloat("PlayerXDist", absoluteX ? Mathf.Abs(playerDist.x) : playerDist.x);
 		animator.SetFloat("PlayerYDist", Mathf.Abs(playerDist.y));
